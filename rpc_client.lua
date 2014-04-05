43d946b3103887fbff7f7940b9a4a47665c46530
Rogério Carvalho Schneider <stockrt@gmail.com>
2014-04-05 19:08:41 -0300

    rpc

--- rpc_client.lua
+++ rpc_client.lua
@@ -4,7 +4,19 @@ local luarpc = require("luarpc")
 
 local arq_interface = "interface.lua"
 
-local proxy1 = luarpc.createproxy (IP, porta1, arq_interface)
-local proxy2 = luarpc.createproxy (IP, porta2, arq_interface)
-local r, s = proxy1.foo(3, 5)
-local t = proxy2.boo(10)
+local proxy1 = luarpc.createproxy(server_ip, server_port, arq_interface)
+local proxy2 = luarpc.createproxy(server_ip, server_port, arq_interface)
+
+-- proxy1/myobj1
+local sum, str = proxy1.foo(5, 3)
+print(sum)
+print(str)
+local res = proxy1.boo(20)
+print("echo " .. res)
+
+-- proxy2/myobj2
+local sub, str = proxy2.foo(5, 3)
+print(sub)
+print(str)
+local res = proxy2.boo(20)
+print("fixed " .. res)
--- rpc_server.lua
+++ rpc_server.lua
@@ -5,8 +5,8 @@ local luarpc = require("luarpc")
 local arq_interface = "interface.lua"
 
 local myobj1 = {
-  foo = function (a, b, s)
-    return a+b, "alo alo"
+  foo = function (a, b, str)
+    return a + b, "alo alo"
   end,
   boo = function (n)
     return n
@@ -14,14 +14,15 @@ local myobj1 = {
 }
 
 local myobj2 = {
-  foo = function (a, b, s)
-    return a-b, "tchau"
+  foo = function (a, b, str)
+    return a - b, "tchau"
   end,
   boo = function (n)
     return 1
   end
 }
 
-local server1 = luarpc.createServant (myobj1, arq_interface)
-local server2 = luarpc.createServant (myobj2, arq_interface)
+local server1 = luarpc.createServant(myobj1, arq_interface)
+local server2 = luarpc.createServant(myobj2, arq_interface)
+
 luarpc.waitIncoming()
