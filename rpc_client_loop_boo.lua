45bfaf80b8bf7eeab9848e499d5f0b99b839c3a4
Rogério Carvalho Schneider <stockrt@gmail.com>
2014-05-03 01:25:11 -0300

    output

--- rpc_client_loop_boo.lua
+++ rpc_client_loop_boo.lua
@@ -13,10 +13,8 @@ local server_address = arg[2]
 local server_port = arg[3]
 local run_seconds = tonumber(arg[4])
 
-
 -- Proxy.
 local proxy = luarpc.createProxy(server_address, server_port, interface_file)
-
 
 -- 1 byte de ida + nome do método = 4 bytes
 -- 1 byte de volta + nil do result padrão = 4 bytes
@@ -27,9 +25,4 @@ while os.difftime(os.time(), tini) < run_seconds do
   local result = proxy.boo(1)
   calls = calls + 1
 end
-local res = "boo calls " .. calls .. " in ".. run_seconds .. " seconds for server " .. server_port
-print(res)
-local file_name = "report/runs/boo.txt"
-local file_handler = io.open(file_name, "a+")
-file_handler:write("\n\n" .. res .. "\n")
-file_handler:close()
+print("boo calls: " .. calls .. " in ".. run_seconds .. " seconds for server " .. server_port)
